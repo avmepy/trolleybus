49ee3dfbfd9451946a35f08d261caabb1da69ab6
Valentine <valentyn.kofanov@knu.ua>
2021-03-04 22:47:28 +0200

    merged to masterC

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
+class Account(models.Model):
+    """
+    extension of the standard User model
+    """
 
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.account.first_name} {self.account.last_name}"
+
+    def __repr__(self):
+        return f"{self.account.first_name} {self.account.last_name}"
+
+    # TODO
--- trolleybus/urls.py
+++ trolleybus/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view()),  # url to get the token
+    path('api/token/refresh/', TokenRefreshView.as_view()),  # url to refresh the token
 ]
