84ea94212fa9971c08cfb7447f3c54c334f17d68
DanyilS <dselivanov2000@gmail.com>
2021-03-06 19:53:32 +0200

    added all needed models, turned on in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Route, Trolleybus, TrolleybusState, StopPoint, Schedule
 
-# Register your models here.
+
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ('route', 'stop_point', 'time')
+
+
+admin.site.register(Trolleybus)
+admin.site.register(Route)
+admin.site.register(TrolleybusState)
+admin.site.register(StopPoint)
+admin.site.register(Schedule, ScheduleAdmin)
