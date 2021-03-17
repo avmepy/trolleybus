b07c2296545f4c17f8753eb85583bb3f525e8f5f
Valentine <valentyn.kofanov@knu.ua>
2021-03-17 19:43:47 +0200

    merged

--- trolleybus/settings.py
+++ trolleybus/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'apps.authentication.backends.JWTAuthentication',
+        'apps.accounts.backends.JWTAuthentication',
     ),
 }
 
