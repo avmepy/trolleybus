c98661ac3178c927fb08bfbaf955420ab4adda66
Valentine <valentyn.kofanov@knu.ua>
2021-03-17 19:54:00 +0200

    bug fixed

--- trolleybus/settings.py
+++ trolleybus/settings.py
@@ -137,6 +137,6 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'project.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'apps.accounts.backends.JWTAuthentication',
+        'accounts.backends.JWTAuthentication',
     ),
 }
