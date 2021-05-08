bde180b2d28372f5f741225078b3dcdaf3018bed
DanyilS <dselivanov2000@gmail.com>
2021-05-08 17:20:38 +0300

    fixed scghedules

--- routes/services.py
+++ routes/services.py
@@ -2,9 +2,10 @@
 # -*-encoding: utf-8-*-
 from django.contrib.auth.models import User
 from routes.models import Shift
+from datetime import date
 
 
 def get_user_shifts(user: User):
 
-    shifts = Shift.objects.filter(driver=user)
+    shifts = Shift.objects.filter(driver=user,  date__gte=date.today())
     return shifts
